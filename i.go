3cd49f9fee5320aad2b3f532c266b70c7747385f
GloftOfficial <lybetsa@live.com>
2022-08-31 17:28:49 +0300

    Update i.go

--- i.go
+++ i.go
@@ -18,9 +18,9 @@ var (
 	// the address to listen on
 	address = "127.0.0.1:9005"
 	// the directory to save the images in
-	root = "/var/www/i.fourtf.com/"
+	root = "/var/www/i.gloft.me/"
 	// the root of the link that will be generated
-	webRoot = "https://i.fourtf.com/"
+	webRoot = "https://i.gloft.me/"
 
 	// maximum age for the files
 	// the program will delete the files older than maxAge every 2 hours
@@ -29,7 +29,7 @@ var (
 	deleteIgnoreRegexp = regexp.MustCompile("index\\.html|favicon\\.ico")
 
 	// length of the random filename
-	randomAdjectivesCount = 2
+	randomAdjectivesCount = 3
 	adjectives            = make([]string, 0)
 	filetypes             = make(map[string]string)
 )
